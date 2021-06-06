7f9352606ed1e9d8e07b97ca67ef61d07630b9da
Rafael Porras <rporrasdominguez@gmail.com>
2021-06-06 14:02:27 +0200

    Creacion tablas de la entidad relacion original

---
+++ database/migrations/2021_06_06_110959_crear_tabla_permiso_rol.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaPermisoRol extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permiso_rol', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('rol_id');
+            $table->foreign('rol_id','fk_permisorol_rol')
+                  ->references('id')
+                  ->on('rol')
+                  ->onUpdate('cascade')
+                  ->onDelete('restrict');
+                  
+            $table->unsignedInteger('permiso_id');
+            $table->foreign('permiso_id','fk_permisorol_permiso')
+                  ->references('id')
+                  ->on('permiso')
+                  ->onUpdate('cascade')
+                  ->onDelete('restrict');
+                  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permiso_rol');
+    }
+}
