97aef98ff418aeefd2fc15c83c42a2aa9b343918
Rafael Porras <rporrasdominguez@gmail.com>
2021-06-12 19:34:09 +0200

    Seeders y Model Factory

---
+++ database/migrations/2021_06_12_160113_campo_nombre_tabla_rol_unico.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CampoNombreTablaRolUnico extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rol', function (Blueprint $table) {
+ 
+            $table->unique('nombre');
+  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rol', function (Blueprint $table) {
+ 
+            $table->dropUnique('nombre');
+  
+        });
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //llama al metodo truncateTablas para eliminar el contenido de las tablas pasadas en el array
+        $this->truncateTablas([
+            'rol',
+            'permiso',
+        ]);
+        
+        //ejecutar el seeder pasado
+        $this->call(TablaRolSeeder::class);
+        $this->call(TablaPermisoSeeder::class);
+    }
+
+    /**
+     * metodo que va a truncar las tablas pasadas en el array
+     * Previamente se desactivan las refencias a las foreign_key 
+     * Se truncan las tablas
+     * Se vuelve a activar las referencias
+     */
+    protected function truncateTablas(array $tablas){
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        foreach($tablas as $tabla){
+            DB::table($tabla)->truncate();
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
